0960d8704304a583dc470ecca0372981d45c0bea
kacper331 <denebv201@gmail.com>
2016-07-07 22:46:53 +0200

    Fix problem with test and add and make some changes in code

--- lib/basket.rb
+++ lib/basket.rb
@@ -5,35 +5,45 @@ class Basket
     @basket = basket
   end
 
-  def add(products, id, warehouses, quantity)
-    find_warehouse_product = warehouses.find { |warehouse| warehouse.product_id == id }
+  def add_product(products, id, warehouses, quantity)
+    find_warehouse_product = find_warehouse(warehouses, id)
     if find_warehouse_product.amount != 0
-      @basket << find_product(products, id)
-      find_warehouse_product.amount -= quantity
+      add_produt_to_basket(products, id, warehouses, quantity)
       puts "Added to basket: #{find_product(products, id).name} in amount of #{quantity}"
     else
       puts "You can't buy #{find_product(products, id).name}, not enough amount in warehouse"
     end
   end
 
-  def remove(products, id, warehouses, quantity)
-    find_warehouse_product = warehouses.find { |warehouse| warehouse.product_id == id }
-    @basket.delete_if { |basket| basket.id == id }
-    find_warehouse_product.amount += quantity
+  def remove_product(products, id, warehouses, quantity)
+    remove_product_from_basket(products, id, warehouses, quantity)
     puts "Removed from basket: #{find_product(products, id).name} in amount of #{quantity}"
   end
 
-  def sum
-    sum = @basket.map { |basket| basket.price }.reduce(0, :+)
+  def add_produt_to_basket(products, id, warehouses, quantity)
+    @basket << find_product(products, id)
+    find_warehouse(warehouses, id).amount -= quantity
   end
 
-  def sum_with_vat
-    sum = @basket.map { |basket| basket.price }.reduce(0, :+)
-    sum_vat = sum * 1.23
+  def remove_product_from_basket(products, id, warehouses, quantity)
+    @basket.delete_if { |basket| basket.id == id }
+    find_warehouse(warehouses, id).amount += quantity
+  end
+
+  def total_price
+    basket.map(&:price).reduce(0, :+)
+  end
+
+  def total_price_with_vat
+    total_price * 1.23
   end
 
   private
   def find_product(products, id)
     find_product = products.find { |product| product.id == id }
+  end
+
+  def find_warehouse(warehouses, id)
+    warehouses.find { |warehouse| warehouse.product_id == id }
   end
 end
--- spec/unit/basket_spec.rb
+++ spec/unit/basket_spec.rb
@@ -2,36 +2,34 @@ require_relative "../../lib/basket.rb"
 require_relative "../../lib/product.rb"
 require_relative "../../lib/warehouse.rb"
 
-
-
 RSpec.describe Basket do
   let(:product) { Product.new(name: "Foo", price: 10.00) }
   let(:products) { [product] }
-  # let(:warehouse) { Warehouse.new(product_id: product.id, amount: 2) }
   let(:warehouses) { Warehouse.new(product_id: product.id, amount: 2) }
-  let(:empty_baskets) { Basket.new }
   let(:quantity) { 1 }
   let(:basket) { Basket.new(products) }
 
-  it "should be a array" do
-    expect(empty_baskets.basket).to eq ([])
-  end
-
-  it "should add a product to basket" do
-    # expect(empty_baskets.add(products, product.id, warehouses, quantity)).to match("Added to basket: #{product.name} in amount of #{quantity}")
-    expect([warehouses]).to receive(:find).and_return(warehouses)
-    Basket.new.add(products, product.id, [warehouses], quantity)
-  end
-
-  context "#sum" do
-    it "should return sum of all products in basket" do
-      expect(basket.sum).to eq(10.00)
+  context "#remove_products_from_basket" do
+    it "should remove products from basket" do
+      expect(basket.remove_product_from_basket(products, product.id, [warehouses], quantity)).to eq(3)
     end
   end
 
-  context "#sum_with_vat" do
+  context "#add_produts_to_basket" do
+    it "should add product to basket" do
+      expect(basket.add_produt_to_basket(products, product.id, [warehouses], quantity)).to eq(1)
+    end
+  end
+
+  context "#total_price" do
+    it "should return sum of all products in basket" do
+      expect(basket.total_price).to eq(10.00)
+    end
+  end
+
+  context "#total_price_with_vat" do
     it "should return sum with VAT of all products in basket" do
-      expect(basket.sum_with_vat).to eq(12.30)
+      expect(basket.total_price_with_vat).to eq(12.30)
     end
   end
 end
