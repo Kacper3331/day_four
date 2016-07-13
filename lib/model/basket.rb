9690d5e5c595d3b5e38096e37c9be79fa42f3b10
kacper331 <denebv201@gmail.com>
2016-07-13 12:42:29 +0200

    Update basket.rb and product.rb

--- lib/model/basket.rb
+++ lib/model/basket.rb
@@ -1,18 +1,35 @@
 module Store
   class Basket
-    attr_reader :product_id, :quantity
+    attr_reader :product_id, :quantity, :id
 
-    def initialize(product_id, quantity)
+    @@id = 0
+
+    def initialize(product_id:, quantity:)
+      @id = set_id
       @product_id = product_id
       @quantity = quantity
     end
 
+    def fetch_product
+      FetchProduct.new.call(product_id)
+    end
+
+    def price_with_vat
+      product = FetchProduct.new.call(product_id)
+      (product.price_netto * 1.23).round(2)
+    end
+
     def total_price
-      selected_products.map(&:price).reduce(0, :+)
+      fetch_product.price_netto * quantity
     end
 
     def total_price_with_vat
-      total_price * 1.23
+      (total_price * 1.23).round(2)
+    end
+
+    private
+    def set_id
+      @@id += 1
     end
   end
 end
--- lib/model/product.rb
+++ lib/model/product.rb
@@ -12,25 +12,9 @@ module Store
       @price_netto = set_price(price_netto)
     end
 
-    def show_product
-      "ID: #{id} \t|\t NAME: #{name} \t|\t PRICE: #{price}"
-    end
-
-    def show_discount_product(amount)
-      "\tNAME: #{name} \t|\t SPECIAL PRICE: #{discount(price, amount)} zl"
-    end
-
-    def discount(price, amount)
-      price -= amount
-    end
-
     def update(parameters)
       @name = parameters[:name] if !parameters[:name].nil?
       @price = parameters[:price] if !parameters[:price].nil?
-    end
-
-    def price_with_vat
-      price * 1.23
     end
 
     private
