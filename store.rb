555e84c578484de931001c9bc83f7ec1dcda8b98
kacper331 <denebv201@gmail.com>
2016-07-13 12:19:08 +0200

    Move store.rb outside of lib folder

---
+++ store.rb
@@ -0,0 +1,53 @@
+require "bundler/setup"
+require "sinatra/base"
+require "pry"
+
+Dir["./lib/**/*.rb"].each{ |file| require file }
+
+module Store
+  PRODUCTS = []
+
+  CreateProduct.new.call("Chocolate", 9.99)
+  CreateProduct.new.call("Milk Natural", 2.00)
+  CreateProduct.new.call("Still Water", 1.68)
+  CreateProduct.new.call("Toilet Paper", 8.88)
+
+  WAREHOUSE = []
+
+  CreateWarehouse.new.call(1, 4)
+  CreateWarehouse.new.call(2, 2)
+  CreateWarehouse.new.call(3, 11)
+  CreateWarehouse.new.call(4, 2)
+
+  BASKET = []
+
+  class App < Sinatra::Base
+
+    get "/" do
+      products = FetchProducts.new.call
+      slim :"products/index", locals: { products: products }
+    end
+
+    get "/products/:id" do |product_id|
+      product = FetchProduct.new.call(product_id)
+      slim :"products/show", locals: { product: product }
+    end
+
+    post "/basket" do
+      AddProductToBasket.new(params).call
+      redirect "/"
+    end
+
+    get "/basket" do
+      products_in_basket = FetchBasket.new.call
+      summary = TotalPriceForProductsInBasket.new.call
+      slim :"basket/show", locals: { basket: products_in_basket, summary: summary }
+    end
+
+    delete "/basket/:id/delete" do
+      RemoveProductFromBasket.new(params[:id]).call
+      redirect "/"
+    end
+
+  end
+end
